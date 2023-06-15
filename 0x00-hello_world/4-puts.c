ec7808890877b8ba2d90ea1a1d30403b8d3d1b22
Mohammed-laaroussi01 <laarouss224@gmail.com>
2023-06-15 09:11:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  main - principal function of the program
+ *
+ * description : it always returns the value 0
+ *
+ *  return : program return value (0)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
