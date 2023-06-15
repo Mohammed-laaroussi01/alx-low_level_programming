cf3b965054db85bfca501abb4baa71615ebc9ba5
Mohammed-laaroussi01 <laarouss224@gmail.com>
2023-06-15 10:13:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,10 @@
  * description : a C program that prints with put fonction
  *
  * return : program return value 0 (Success)
- */
+*/
 int main(void)
 {
-/* code of program */
-puts("\"Programming is like building a multilingual puzzle\"");
-return (0);
+	/* code of program */
+	puts("\"Programming is like building a multilingual puzzle\"");
+	return (0);
 }
