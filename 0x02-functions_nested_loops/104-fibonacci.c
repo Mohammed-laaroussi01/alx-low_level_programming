5f017e4696bbc88ea20a52c813567412246e0ce3
Mohammed-laaroussi01 <laarouss224@gmail.com>
2023-06-21 14:58:09 -0700

    rfr

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description:prints the first 98 Fibonacci numbers, starting
+ *		with 1 and 2
+ * Return: 0
+ */
+int main(void)
+{
+	int count = 98;
+	int num1 = 1;
+	int num2 = 2;
+	int sum;
+	int i;
+
+	printf("%d, %d, ", num1, num2);
+
+	for (i = 3; i <= count; i++)
+	{
+	sum = num1 + num2;
+	printf("%d", sum);
+
+	if (i < count)
+	{
+		printf(", ");
+	}
+	num1 = num2;
+	num2 = sum;
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
