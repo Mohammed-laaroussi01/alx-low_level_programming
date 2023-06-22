2e0f92caa282c34f29a473f87800946ca8092b3a
Mohammed-laaroussi01 <laarouss224@gmail.com>
2023-06-22 07:35:02 -0700

    void print_numbers(void)

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		return (i);
+	}
+	_putchar('\n');
+}
