6c56839e4a4c64c6cf9c65858aa907373840b176
Mohammed-laaroussi01 <laarouss224@gmail.com>
2023-06-22 07:54:04 -0700

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+void print_most_numbers(void)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+}
