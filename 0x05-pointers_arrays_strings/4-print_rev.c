75ebb633f33837ee5424389a5c865c555c3a6624
Mohammed-laaroussi01 <laarouss224@gmail.com>
2023-06-26 09:21:03 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	{
+	 while (str[i] != '0')
+		 
+		 i--;
+	 _putchar(i + '0');
+	}
+	 _putchar('\n');
+}
